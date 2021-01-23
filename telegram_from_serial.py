2705a12601226c901f297318acb69663dfac7129
Pim van den Berg <pim@nethuis.nl>
2021-01-23 19:45:32 +0100

    add support to write to influxdb every 60s
    These environment variables can be set:
    - INFLUXDB_HOST (Default: localhost)
    - INFLUXDB_PORT (Default: 8086)
    - INFLUXDB_USERNAME (Default: root)
    - INFLUXDB_PASSWORD (Default: root)
    - INFLUXDB_DATABASE (Default: dsmr)

--- telegram_from_serial.py
+++ telegram_from_serial.py
@@ -6,38 +6,43 @@ import sys
 import serial
 import crcmod.predefined
 import datetime
+import time
+import os
 
 # Debugging settings
 production = True   # Use serial or file as input
-debugging = 1   # Show extra output
+debugging = 0   # Show extra output
 # DSMR interesting codes
 list_of_interesting_codes = {
-    '1-0:1.8.1': 'Meter Reading electricity delivered to client (Tariff 1) in kWh',
-    '1-0:1.8.2': 'Meter Reading electricity delivered to client (Tariff 2) in kWh',
-    '1-0:2.8.1': 'Meter Reading electricity delivered by client (Tariff 1) in kWh',
-    '1-0:2.8.2': 'Meter Reading electricity delivered by client (Tariff 2) in kWh',
-    '0-0:96.14.0': 'Tariff indicator electricity',
-    '1-0:1.7.0': 'Actual electricity power delivered (+P) in kW',
-    '1-0:2.7.0': 'Actual electricity power received (-P) in kW',
-    '0-0:17.0.0': 'The actual threshold electricity in kW',
-    '0-0:96.3.10': 'Switch position electricity',
-    '0-0:96.7.21': 'Number of power failures in any phase',
-    '0-0:96.7.9': 'Number of long power failures in any phase',
-    '1-0:32.32.0': 'Number of voltage sags in phase L1',
-    '1-0:52.32.0': 'Number of voltage sags in phase L2',
-    '1-0:72:32.0': 'Number of voltage sags in phase L3',
-    '1-0:32.36.0': 'Number of voltage swells in phase L1',
-    '1-0:52.36.0': 'Number of voltage swells in phase L2',
-    '1-0:72.36.0': 'Number of voltage swells in phase L3',
-    '1-0:31.7.0': 'Instantaneous current L1 in A',
-    '1-0:51.7.0': 'Instantaneous current L2 in A',
-    '1-0:71.7.0': 'Instantaneous current L3 in A',
-    '1-0:21.7.0': 'Instantaneous active power L1 (+P) in kW',
-    '1-0:41.7.0': 'Instantaneous active power L2 (+P) in kW',
-    '1-0:61.7.0': 'Instantaneous active power L3 (+P) in kW',
-    '1-0:22.7.0': 'Instantaneous active power L1 (-P) in kW',
-    '1-0:42.7.0': 'Instantaneous active power L2 (-P) in kW',
-    '1-0:62.7.0': 'Instantaneous active power L3 (-P) in kW'
+    '1-0:1.8.1': {'tag': 'electricity_used_tariff1', 'description': 'Meter Reading electricity delivered to client (Tariff 1) in kWh'},
+    '1-0:1.8.2': {'tag': 'electricity_used_tariff2', 'description': 'Meter Reading electricity delivered to client (Tariff 2) in kWh'},
+    '1-0:2.8.1': {'tag': 'electricity_delivered_tariff1', 'description': 'Meter Reading electricity delivered by client (Tariff 1) in kWh'},
+    '1-0:2.8.2': {'tag': 'electricity_delivered_tariff2', 'description': 'Meter Reading electricity delivered by client (Tariff 2) in kWh'},
+    '0-0:96.14.0': {'tag': 'active_tariff', 'description': 'Tariff indicator electricity'},
+    '1-0:1.7.0': {'tag': 'current_electricity_usage', 'description': 'Actual electricity power delivered (+P) in kW'},
+    '1-0:2.7.0': {'tag': 'current_electricity_delivery', 'description': 'Actual electricity power received (-P) in kW'},
+    '0-0:17.0.0': {'tag': 'threshold', 'description': 'The actual threshold electricity in kW'},
+    '0-0:96.3.10': {'tag': 'switch_position', 'description': 'Switch position electricity'},
+    '0-0:96.7.21': {'tag': 'short_failure_count', 'description': 'Number of power failures in any phase'},
+    '0-0:96.7.9': {'tag': 'long_failure_count', 'description': 'Number of long power failures in any phase'},
+    '1-0:32.32.0': {'tag': 'voltage_sag_l1_count', 'description': 'Number of voltage sags in phase L1'},
+    '1-0:52.32.0': {'tag': 'voltage_sag_l2_count', 'description': 'Number of voltage sags in phase L2'},
+    '1-0:72:32.0': {'tag': 'voltage_sag_l3_count', 'description': 'Number of voltage sags in phase L3'},
+    '1-0:32.36.0': {'tag': 'voltage_swell_l1_count', 'description': 'Number of voltage swells in phase L1'},
+    '1-0:52.36.0': {'tag': 'voltage_swell_l2_count', 'description': 'Number of voltage swells in phase L2'},
+    '1-0:72.36.0': {'tag': 'voltage_swell_l3_count', 'description': 'Number of voltage swells in phase L3'},
+    '1-0:32.7.0': {'tag': 'voltage_l1', 'description': 'Instantaneous voltage L1 in V'},
+    '1-0:52.7.0': {'tag': 'voltage_l2', 'description': 'Instantaneous voltage L2 in V'},
+    '1-0:72.7.0': {'tag': 'voltage_l3', 'description': 'Instantaneous voltage L3 in V'},
+    '1-0:31.7.0': {'tag': 'current_l1', 'description': 'Instantaneous current L1 in A'},
+    '1-0:51.7.0': {'tag': 'current_l2', 'description': 'Instantaneous current L2 in A'},
+    '1-0:71.7.0': {'tag': 'current_l3', 'description': 'Instantaneous current L3 in A'},
+    '1-0:21.7.0': {'tag': 'active_positive_l1', 'description': 'Instantaneous active power L1 (+P) in kW'},
+    '1-0:41.7.0': {'tag': 'active_positive_l2', 'description': 'Instantaneous active power L2 (+P) in kW'},
+    '1-0:61.7.0': {'tag': 'active_positive_l3', 'description': 'Instantaneous active power L3 (+P) in kW'},
+    '1-0:22.7.0': {'tag': 'active_negative_l1', 'description': 'Instantaneous active power L1 (-P) in kW'},
+    '1-0:42.7.0': {'tag': 'active_negative_l2', 'description': 'Instantaneous active power L2 (-P) in kW'},
+    '1-0:62.7.0': {'tag': 'active_negative_l3', 'description': 'Instantaneous active power L3 (-P) in kW'},
 }
 
 # Program variables
@@ -66,6 +71,29 @@ else:
     print("Running in test mode")
     # Testing
     ser = open("raw.out", 'rb')
+
+def export_to_influxdb(fields):
+  from influxdb import InfluxDBClient
+
+  influx_client = InfluxDBClient(
+    host=os.getenv('INFLUXDB_HOST', 'localhost'),
+    port=os.getenv('INFLUXDB_PORT', 8086),
+    username=os.getenv('INFLUXDB_USERNAME', 'root'),
+    password=os.getenv('INFLUXDB_PASSWORD', 'root'),
+    database=os.getenv('INFLUXDB_DATABASE', 'dsmr')
+  )
+
+  json_body = [
+    {
+      "measurement": "dsmr",
+      "time": datetime.datetime.utcnow().replace(microsecond=0).isoformat(),
+      "fields": fields,
+    }
+  ]
+  try:
+    influx_client.write_points(json_body)
+  except Exception as e:
+    print('Failed to write to influxdb: ', e)
 
 while True:
     try:
@@ -144,11 +172,15 @@ while True:
                 telegram_values[code] = value
 
         # Print the lines to screen
+        influx_input = {}
         for code, value in sorted(telegram_values.items()):
             code = code.decode('ascii')
             if code in list_of_interesting_codes:
-                value = float(value.lstrip(b'\(').rstrip(b'\)*kWhA'))
-                # Print nicely formatted string
-		if debugging > 0:
-			print(datetime.datetime.utcnow()), 
-                print("{0:<63}{1:>12}".format(list_of_interesting_codes[code], value))
+                value = float(value.lstrip(b'\(').rstrip(b'\)*kWhAV'))
+                influx_input[list_of_interesting_codes[code]['tag']] = value
+
+        export_to_influxdb(influx_input)
+        time.sleep(60 - time.time() % 60)
+    else:
+        if debugging == 1:
+            print("Bad checksum")
